5e4b440f3ee00d129b1741b5320d82568124aba6
zhuguo2011-tech <zhuguo2011@gmail.com>
2026-08-14 12:41:02 +0800

    Delete app directory

--- app/src/main/java/com/nanjing/photoapp/model/Photo.kt
+++
@@ -1,9 +0,0 @@
-package com.nanjing.photoapp.model
-
-data class Photo(
-    val id: Int,
-    val filename: String,
-    val original_name: String?,
-    val uploaded_at: String?,
-    val url: String
-)
